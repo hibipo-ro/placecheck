dc2e6e239695132ad16b7e9c3c3a285651d10028
Hibiki Nitta <hi_bi_0120@icloud.com>
2019-05-19 15:25:35 +0900

    ture を trueに直した

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
-  validates :name, presence: ture, length: { maximum: 50 }
-  validates :email, presence: ture, length: { maximum: 255 },
+  validates :name, presence: true, length: { maximum: 50 }
+  validates :email, presence: true, length: { maximum: 255 },
                     uniqueness: true
   has_secure_password
   validates :password, presence: true, length: { minimum: 6 }
