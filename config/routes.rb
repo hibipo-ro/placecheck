e3e84152d46b56c90442ab9fde22497a26c16eab
Hibiki Nitta <hi_bi_0120@icloud.com>
2019-06-23 18:23:19 +0900

    commentモデル追加

--- config/routes.rb
+++ config/routes.rb
@@ -17,6 +17,9 @@ Rails.application.routes.draw do
       get :following, :followers
     end
   end  
-  resources :posts        
+  resources :posts do
+    resources :comments
+  end
+       
   resources :relationships,       only: [:create, :destroy]
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190622080541) do
+ActiveRecord::Schema.define(version: 20190623090847) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "comments", force: :cascade do |t|
+    t.string "body"
+    t.bigint "post_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["post_id"], name: "index_comments_on_post_id"
+  end
 
   create_table "posts", force: :cascade do |t|
     t.text "content"
@@ -52,5 +60,6 @@ ActiveRecord::Schema.define(version: 20190622080541) do
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
+  add_foreign_key "comments", "posts"
   add_foreign_key "posts", "users"
 end
