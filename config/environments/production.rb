2d70241100de41b43ad050bfc834595310795ad4
Hibiki Nitta <hi_bi_0120@icloud.com>
2019-07-02 14:59:41 +0900

    治ったけどどこがおかしいのかわからないからtry

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -89,5 +89,5 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.assets.precompile += ['posts.coffee']
+  # config.assets.precompile += ['posts.coffee']
 end
