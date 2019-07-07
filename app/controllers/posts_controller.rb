b45837cd09eb1e0b68255023e886ad17f201b6f8
Hibiki Nitta <hi_bi_0120@icloud.com>
2019-07-07 18:49:43 +0900

    画像を削除できるようにした

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -45,7 +45,7 @@ class PostsController < ApplicationController
     
     def post_params
       params.require(:post).permit(:content, :title,
-                                   :data1, :data2, :data3, :data4, :data5, :picture)
+                                   :data1, :data2, :data3, :data4, :data5, :picture,:remove_picture)
     end
 
     #正しいユーザーか確認
