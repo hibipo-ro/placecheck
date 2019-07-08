c577bee8314a4407190290ca5a3af3faf7633c1d
Hibiki Nitta <hi_bi_0120@icloud.com>
2019-07-08 17:27:49 +0900

    投稿した人の写真が表示できていなかったので修正

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -4,8 +4,8 @@ class StaticPagesController < ApplicationController
       @post  = current_user.posts.build
       @feed_items = current_user.feed.paginate(page: params[:page])
     end
-    @user = current_user
-    @posts = @user.feed.all
+    user = current_user
+    @posts = user.feed.all
   end
 
   def help
